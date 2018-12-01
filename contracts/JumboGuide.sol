1a658af9cf498505572b4c75221f47257ce450b9
Sang-Hyeon Park <twodude@naver.com>
2018-12-02 03:53:41 +0900

    Update JumboGuide.sol

--- contracts/JumboGuide.sol
+++ contracts/JumboGuide.sol
@@ -1,14 +1,5 @@
-interface JumboGuide {
-    enum Categories {Star3, Star2, Star1, Korean, Chinese, Japanese, Russian}
-    
-    struct Restaurant {
-        bytes32 image;
-        string name;
-        string location;
-        Categories category;
-        string menu;
-        int256 score;
-    }
+contract JumboGuide {
+    enum Categories {Korean, Chinese, Japanese, Russian}
     
     struct Review {
         address writer;
@@ -17,23 +8,68 @@ interface JumboGuide {
         int256 score;
     }
     
+    struct Restaurant {
+        bytes32 image;
+        string name;
+        string location;
+        Categories category;
+        uint8 numJumbo;
+        int256 score;
+    }
+    
+    // hash(Restaurant) => Review[]
+    mapping(bytes32 => Review[]) reviews;    
+    
+    mapping(address => Restaurant) rests;
+    
+    Restaurant _tmp;
+    
     function registerRestaurant(
-        bytes32 image,
-        string calldata name,
-        string calldata location,
-        Categories category,
-        string calldata menu
-        ) external;
-    function voteRestaurant(address store, bool upvote) external;
-    function getRestaurants(Categories category) external returns(address[] memory);
-    function getRestaurant(address store) external returns(
+        bytes32 _image,
+        string memory _name,
+        string memory _location,
+        Categories _category
+    ) public {
+        Restaurant storage tmp = _tmp;
+        
+        tmp.image = _image;
+        tmp.name = _name;
+        tmp.location = _location;
+        tmp.category = _category;
+        tmp.numJumbo = 0;
+        tmp.score = 0;
+        
+        rests[msg.sender] = tmp;
+    }
+        
+    function getRestaurant(address store)
+    public
+    returns(
         bytes32 image,
         string memory name,
         string memory location,
         Categories category,
-        string memory menu,
+        uint8 numJumbo,
         int256 score
+    ) {
+        
+        Restaurant memory tmp = rests[store];
+        
+        return (
+            tmp.image,
+            tmp.name,
+            tmp.location,
+            tmp.category,
+            tmp.numJumbo,
+            tmp.score
         );
+    }
+        
+        
+    /*
+    function voteRestaurant(address store, bool upvote) external;
+    function getRestaurants(Categories category) external returns(address[] memory);
+
     
     function voteRestaurantWithReview(address store, bytes32 hash, int256 score) external;
     function voteReview(uint256 reviewId, bool upvote) external;
@@ -44,4 +80,5 @@ interface JumboGuide {
         string memory body,
         int256 score
         );
+    */
 }
