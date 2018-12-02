9cbccd5c3d97572a3a263352024183cdb8b2883e
Sang-Hyeon Park <twodude@naver.com>
2018-12-02 11:22:46 +0900

    Update MyJumbo.sol

--- contracts/MyJumbo.sol
+++ contracts/MyJumbo.sol
@@ -1,29 +1,62 @@
 contract MyJumbo {
-    enum States {Normal, Hunger, Hangry}
-    enum Colors {Red, Green, Blue, Black, White, Rainbow}
+    // enum States {Normal, Hunger, Hangry}
+    enum Colors {Normal, Red, Green, Blue, Black, White, Rainbow}
+    uint8 numColors = 7;
     
     struct Jumbo {
         uint8 level;
-        States state;
         bytes32[] items;
-        Colors[] berries;
-        Colors jumboColor;
-        Colors lastBerry;
+        Colors jumboColor;  // lastBerry
+        uint256[] berries;
+        // States state;
+        // Colors lastBerry;
     }
     
+    mapping(address => Jumbo) myjumbo;
     // mapping(address => bytes32[]) public myItems;
     
-    function feed(Colors berry, uint8 amount) external;
+    function createMyJumbo() public {
+        Jumbo storage tmp = myjumbo[msg.sender];
+        tmp.level = 1;
+        tmp.jumboColor = Colors.Normal;
+        tmp.berries = new uint256[](numColors);
+    }
+    
+    function feed(Colors berry, uint256 amount) public {
+        (myjumbo[msg.sender].berries)[uint(berry)] += amount;
+        
+        uint256 res = 0;
+        Colors tar = Colors(0);
+        for (uint8 i=0; i<numColors; i++){
+            if((myjumbo[msg.sender].berries)[i] > res) {
+                res = (myjumbo[msg.sender].berries)[i];
+                tar = Colors(i);
+            }
+        }
+        
+        myjumbo[msg.sender].jumboColor = tar;
+    }
+    
+    function getJumbo(address user)
+    public
+    returns(
+        uint8 level,
+        bytes32[] memory items,
+        Colors jumboColor,
+        uint256[] memory berries
+    ) {
+        return(
+            myjumbo[user].level,
+            myjumbo[user].items,
+            myjumbo[user].jumboColor,
+            myjumbo[user].berries
+        );
+    }
+    
+    /*
     function equipItem(bytes32 itemId) external;
     function removeItem(bytes32 itemId) external;
     // function getItems() external returns(bytes32[] lists);
     function getItems() external returns(bytes32[] memory lists);
-    function getJumbo() external returns(
-        uint8 level,
-        States state,
-        bytes32[] memory items,
-        Colors[] memory berries,
-        Colors jumboColor,
-        Colors lastBerry
-    );
+    */
 }
