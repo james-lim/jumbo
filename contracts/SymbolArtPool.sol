8d4c20f203ab535444017dbe9fd58defebd94dc4
Sang-Hyeon Park <twodude@naver.com>
2018-12-02 12:45:12 +0900

    Update SymbolArtPool.sol

--- contracts/SymbolArtPool.sol
+++ contracts/SymbolArtPool.sol
@@ -1,12 +1,34 @@
-interface SymbolArtPool {
-    // SymbolPool
+contract SymbolArtPool {
+    mapping(address => bytes32) symbols;
+    mapping(bytes32 => uint256) priceTag;
+    mapping(bytes32 => address payable) owners;
     
-    // mapping(bytes32 => address) public owners;
-    // mapping(address => bytes32) public mySymbols;
+    bytes32[] pool;
     
-    function registerSymbol(bytes32 hash, uint256 price) external;
-    function buy(bytes32 hash) external payable;
-    function getSymbol(address store) external returns(bytes32 hash);
-    // function getSymbols() external returns(bytes32[] lists);
-    function getSymbols() external returns(bytes32[] memory lists);
+    function registerSymbol(bytes32 hash, uint256 price) public {
+        priceTag[hash] = price;
+        owners[hash] = msg.sender;
+        pool.push(hash);
+    }
+    
+    function buy(bytes32 hash)
+    public
+    payable {
+        if(msg.value >= priceTag[hash]){
+            owners[hash].transfer(msg.value);
+            symbols[msg.sender] = hash;
+        }
+    }
+    
+    function getSymbolForStore(address store) public view returns(bytes32 hash) {
+        return symbols[store];
+    }
+    
+    function getSymbol(bytes32 hash) public returns(uint256 price, address owner) {
+        return (priceTag[hash], owners[hash]);
+    }
+    
+    function getSymbols() public returns(bytes32[] memory lists) {
+        return pool;
+    }
 }
